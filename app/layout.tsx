c55e3a0cb4b4215e16348501f8adcea331e04198
Hostel Pulse Developer <developer@hostelpulse.com>
2026-09-11 10:37:43 +0100

    fix: remove duplicate favicon tags in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -104,8 +104,6 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
             <head>
-                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-                                                <link rel="apple-touch-icon" href="/favicon.svg" />
                 <Script 
                     src="https://checkout.flutterwave.com/v3.js" 
                     strategy="beforeInteractive"
